517231382060a826e2109c2eb65a4fde4379dd41
Alyona <oranchuka@gmail.com>
2025-08-25 23:15:26 +0300

    gallery

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+
 new Swiper('.image-slider', {
-    navigator: {
+    slidesPerView: 3,
+    spaceBetween: 9.5,
+    centeredSlides: false,
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+        slidesOffsetBefore: 50,
+  slidesOffsetAfter: 50,
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 2.5,
+            spaceBetween: 8,
+        },
+       
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 9.5,
+        }
+    }
 });
