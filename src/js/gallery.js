5a421622f5673e12cb0d26d9e8a23389523b472d
Alyona <oranchuka@gmail.com>
2025-08-25 23:55:44 +0300

    js-gallery

---
+++ src/js/gallery.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Swiper('.image-slider', {
+    slidesPerView: 3,
+    spaceBetween: 9.5,
+    centeredSlides: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        slidesOffsetBefore: 50,
+  slidesOffsetAfter: 50,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2.5,
+            spaceBetween: 8,
+        },
+       
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 9.5,
+        }
+    }
+});
