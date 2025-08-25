99dbc7fe87d36527ea70b20c26e8226a40eb4d7e
Alyona <oranchuka@gmail.com>
2025-08-26 00:12:55 +0300

    gallery-html

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,25 +1,30 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-
-new Swiper('.image-slider', {
+new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 9.5,
     centeredSlides: false,
+    slidesOffsetBefore: 50, 
+    slidesOffsetAfter: 50, 
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-        slidesOffsetBefore: 50,
-  slidesOffsetAfter: 50,
     },
     breakpoints: {
         320: {
             slidesPerView: 2.5,
             spaceBetween: 8,
+            slidesOffsetBefore: 0, 
+            slidesOffsetAfter: 0,
+            navigation: false,     
         },
-       
         1200: {
             slidesPerView: 3,
             spaceBetween: 9.5,
+            slidesOffsetBefore: 50,
+            slidesOffsetAfter: 50,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
         }
     }
 });
