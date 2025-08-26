da3e0736ae49523fa06c35e3e617b345a4233cb4
Alyona <oranchuka@gmail.com>
2025-08-26 17:51:46 +0300

    fix-js-gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,30 +1,31 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 9.5,
-    centeredSlides: false,
-    slidesOffsetBefore: 50, 
-    slidesOffsetAfter: 50, 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  modules: [Navigation],
+  centeredSlides: false,
+  watchOverflow: true,
+
+  slidesPerView: 2.5,
+  spaceBetween: 8,
+
+  navigation: {
+    enabled: false, 
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      1200: {
+         
+    slidesOffsetAfter: 60,
+      slidesPerView: 3,
+      spaceBetween: 9.5,
+      navigation: {
+        enabled: true, 
+      },
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 2.5,
-            spaceBetween: 8,
-            slidesOffsetBefore: 0, 
-            slidesOffsetAfter: 0,
-            navigation: false,     
-        },
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 9.5,
-            slidesOffsetBefore: 50,
-            slidesOffsetAfter: 50,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        }
-    }
+  },
 });
